3e3218d0e8caa553448355ad346e21ce29980f46
K.Stepanov <unholy.vostok@gmail.com>
2016-05-18 01:59:07 +0300

    Added a ratings&finished_on rows, #finished?, css-color for #finished?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
-Book.create!([{
+Book.create!([
+{
 	title: "Hyperion",
 	author: "Dan Simmons",
 	description: "Probably my favorite science fiction book (and series) I've ever read.",
-	amazon_id: "0553283685"
-}])
+	amazon_id: "0553283685",
+	rating: 5,
+	finished_on: 10.days.ago
+},
+{
+	title: "Example Book",
+	author: "Noname",
+	description: "I don't know what is it. Really.",
+	amazon_id: "055321231",
+	rating: 1,
+	finished_on: nil
+}
+])
