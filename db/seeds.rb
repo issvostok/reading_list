ca035ff287ecae5b435cd5d7588d3a1d75cd4eb4
K.Stepanov <unholy.vostok@gmail.com>
2016-05-18 01:13:43 +0300

    Added a root routes & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([{
+	title: "Hyperion",
+	author: "Dan Simmons",
+	description: "Probably my favorite science fiction book (and series) I've ever read.",
+	amazon_id: "0553283685"
+}])
